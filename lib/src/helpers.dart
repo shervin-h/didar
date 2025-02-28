88105c9eed54de204a04e640614fb76bf240e66b
shervin.hassanzadeh <shervin.hz07@gmail.com>
2025-02-28 14:22:19 -0800

    Add ic launcher - Configurations for getting APK output

--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -1,4 +1,14 @@
 extension DurationExtensions on Duration {
+  /// Converts the duration to a `MM:SS` formatted string.
+  ///
+  /// - Minutes (`MM`) and seconds (`SS`) are always two digits, with leading zeros if needed.
+  /// - Example:
+  ///   ```dart
+  ///   Duration(seconds: 65).toMMSS(); // Returns "01:05"
+  ///   Duration(seconds: 9).toMMSS();  // Returns "00:09"
+  ///   ```
+  ///
+  /// Returns a string representation of the duration in `MM:SS` format.
   String toMMSS() {
     final minutes = inMinutes.remainder(60).toString().padLeft(2, '0');
     final seconds = inSeconds.remainder(60).toString().padLeft(2, '0');
